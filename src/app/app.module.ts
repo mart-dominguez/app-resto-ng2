53124bc9a510e394f6feb066667ac09100e492b7
mart-dominguez <mdomingu@gmail.com>
2017-10-24 06:56:00 -0300

    asa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 
+import { RouterModule, Routes } from '@angular/router'; 
+
 import { AppComponent } from './app.component';
 import { PlatoModule } from './plato/plato.module'
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MenuComponent } from './shared/components/menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { AdmPlatoComponent} from './plato/adm-plato/adm-plato.component';
+
+const routes: Routes = [ 
+  {path: '', redirectTo: 'home', pathMatch: 'full'}, 
+  {path: 'home', component: HomeComponent}, 
+  {path: 'platos', component: AdmPlatoComponent} 
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     NgbModule.forRoot(),
     BrowserModule,
     FormsModule,
-    PlatoModule    
+    PlatoModule,
+    RouterModule.forRoot(routes) 
   ],
   providers: [],
   bootstrap: [AppComponent]
